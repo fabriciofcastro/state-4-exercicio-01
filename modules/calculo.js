7951e53f5deb87d0404549df140ecf4cca9056d6
Fabricio Castro <fabriciofeitosacastro@gmail.com>
2023-10-25 13:19:10 -0300

    finalizado

---
+++ modules/calculo.js
@@ -0,0 +1,29 @@
+export function adcao(numberOne, numberTwo) {
+  let result = numberOne + numberTwo
+
+  alert(`Resultado da adcao é ${result}`) 
+  return result
+}
+
+export function multiplicacao(numberOne, numberTwo) {
+  let result = numberOne * numberTwo
+
+  alert(`Resulçtado da divisão é ${result}`)
+  return result
+}
+
+export function divisao(numberOne, numberTwo) {
+  let result = numberOne / numberTwo
+  let resto = numberOne % numberTwo
+
+  alert(`Resultado da divisão ${result} e, o resto da divisão é ${resto}` )
+  return result
+}
+
+export function subtracao(numberOne, numberTwo) {
+  let result = numberOne - numberTwo
+
+  alert(`Resultado da multiplicacao ${result}`)
+  return result
+}
+
---
+++ modules/index.js
@@ -0,0 +1,56 @@
+import { adcao, multiplicacao, divisao, subtracao } from './calculo.js'
+
+const printResult = document.querySelector(".resultado")
+
+const btn = document.querySelector( '#enviar' )
+
+btn.addEventListener( "click", function ( e )
+{
+  e.preventDefault()
+
+  let select = document.getElementById( 'opcoes-conta' )
+  let value = select.value
+
+  const number1= document.querySelector( '#numberOne' )
+  const number2 = document.querySelector( '#numberTwo' )
+
+  let numberOne = Number(number1.value)
+  let numberTwo = Number(number2.value)
+
+  
+  console.log( value );
+
+  if ( numberOne === numberTwo ) {
+    alert( `Os números digitados Número ${ numberOne } é igual a o Número ${ numberTwo }` )
+  } else {
+    alert( `Os valores digitados Número ${ numberOne } não é igual a o Número ${ numberTwo }` )
+  }
+
+
+  switch ( value ) {
+    case 'adcao': {
+      printResult.innerHTML = adcao( numberOne, numberTwo )
+      
+      break
+    }
+
+    case 'subtracao': {
+      subtracao( numberOne, numberTwo )
+      break
+    }
+
+    case 'divisao': {
+      divisao( numberOne, numberTwo )
+      break
+    }
+
+    case 'multiplicacao': {
+      multiplicacao( numberOne, numberTwo )
+      break
+    }
+
+    default:
+      alert( `Opção iválida!` )
+      break;
+  }
+} )
